c0c246c1257a7a14da28d1825cb16a355950f3ea
SCKboss <=>
2023-08-31 15:57:44 +0700

    lab 15

--- src/app/page.js
+++ src/app/page.js
@@ -44,25 +44,34 @@ const schema = z
     }),
     hasCoupon: z.boolean(),
     coupon: z.string(),
-    password: z.string(),
+
+    password: z
+      .string()
+      .min(6, { message: "Password must contain at least 6 characters" })
+      .max(12, { message: "Password must not exceed 12 characters" }),
     confirmPassword: z.string(),
   })
   .refine(
-    //refine let you check error in your own way
-    //in this example, we check "hasCoupon" with "coupon" fields
     (data) => {
       // if user does not tick "I have coupon", then it's ok
       if (!data.hasCoupon) return true;
       // if user tick "I have coupon" and fill correct code, then it's ok too
       if (data.hasCoupon && data.coupon === "CMU2023") return true;
       // ticking "I have coupon" but fill wrong coupon code, show error
-      return false;
+      if (data.hasCoupon && data.coupon !== "CMU2023") return false;
     },
     //set error message and the place it should show
     {
       message: "Invalid coupon code",
       path: ["coupon"],
     }
+  )
+  .refine(
+    (data) => {
+      if (data.confirmPassword === data.password) return true;
+      return false;
+    },
+    { message: "PassWord does not match", path: ["confirmPassword"] }
   );
 
 export default function Home() {
@@ -90,6 +99,12 @@ export default function Home() {
     //TIP : get value of currently filled form with variable "form.values"
 
     if (form.values.plan === "funrun") price = 500;
+    if (form.values.plan === "mini") price = 800;
+    if (form.values.plan === "half") price = 1200;
+    if (form.values.plan === "full") price = 1500;
+
+    if (form.values.hasCoupon && form.values.coupon === "CMU2023") price *= 0.7;
+
     //check the rest plans by yourself
     //TIP : check /src/app/libs/runningPlans.js
 
@@ -175,7 +190,11 @@ export default function Home() {
           </Stack>
         </form>
 
-        <Footer year={2023} fullName="Chayanin Suatap" studentId="650610560" />
+        <Footer
+          year={2023}
+          fullName="Boonyawut Buthboon"
+          studentId="650612088"
+        />
       </Container>
 
       <TermsAndCondsModal opened={opened} close={close} />
